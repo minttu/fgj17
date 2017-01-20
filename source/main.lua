7d91c581a9d2c4105cad28ad6074643731fb1354
logarithm <allan.palmu@gmail.com>
2017-01-20 21:49:51 +0200

    hd 720p graphics added omg

--- source/main.lua
+++ source/main.lua
@@ -2,6 +2,7 @@ gamestate = require "hump.gamestate"
 helloWorld = require "helloworld"
 
 function love.load()
+    love.window.setMode(1280, 720, {})
     gamestate.registerEvents()
     gamestate.switch(helloWorld)
 end
