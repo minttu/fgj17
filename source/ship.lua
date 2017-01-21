96cc17c3de3d0fb8bd68d74a90da087f2d58cb4e
Tuomas Kinnunen <tuomas.kinnunen@aalto.fi>
2017-01-21 19:18:17 +0200

    quaternion derp removed

--- source/ship.lua
+++ source/ship.lua
@@ -2,6 +2,7 @@ require "util"
 cpml = require "cpml"
 quat = cpml.quat
 vec3 = cpml.vec3
+vector = require "hump.vector"
 Sounds = require "sounds"
 
 local Ship = {}
@@ -17,31 +18,34 @@ Ship.turnspeed = 0
 function Ship.new(x, y)
     local self = setmetatable({}, Ship)
     self.location = cpml.vec2.new(x, y)
-    self.orientation = quat.from_angle_axis(0, vec3.unit_z)
+    self.orientation = vector(0,0) -- roll, pitch --quat.from_angle_axis(0, vec3.unit_z)
+    self.yaw = 0
     return self
 end
 
 function Ship.angle(self)
-    local rot = self.orientation
-    ang = rot:to_vec3()
-    return -math.atan2(ang.y, ang.x)
+    --local rot = self.orientation
+    --ang = rot:to_vec3()
+    --return -math.atan2(ang.y, ang.x)
+    return self.yaw
 end
 
 
 function Ship:getRoll()
-    local rot = self.orientation:to_vec3()
-    local ang = math.atan2(rot.y, rot.z)
-    return fixAtan2Angle(ang)
+    --local rot = self.orientation:to_vec3()
+    --local ang = math.atan2(rot.y, rot.z)
+    --return fixAtan2Angle(ang)
+    return self.orientation.x
 end
 function Ship:getPitch()
-    local rot = self.orientation:to_vec3()
-    local ang = math.atan2(rot.x, rot.z)
-    return fixAtan2Angle(ang)
+    --local rot = self.orientation:to_vec3()
+    --local ang = math.atan2(rot.x, rot.z)
+    --return fixAtan2Angle(ang)
+    return self.orientation.y
 end
 
 function Ship.updateLocation(self, dt)
 
-    rot = self.orientation
 
     if love.keyboard.isDown( "left" ) then
         self.turnspeed = self.turnspeed - self.turnrate*dt
@@ -59,22 +63,13 @@ function Ship.updateLocation(self, dt)
 
     --self.turnspeed = self.turnspeed / 1.01 -- slowly normalize
 
-    turn = quat.from_angle_axis(self.turnspeed, cpml.vec3.unit_z)
-    rot = rot * turn
+    --rot = self.orientation
+    --turn = quat.from_angle_axis(self.turnspeed, cpml.vec3.unit_z)
+    --rot = rot * turn
 
-    self.orientation = rot
-    local pitch = self:getPitch()
-    local roll = self:getRoll()
-    print(rot)
-    print(pitch)
-    print(roll)
-
-    local pRot = quat.from_angle_axis(math.sin(pitch) * 0.001* dt*dt, vec3.unit_x)
-    local rRot = quat.from_angle_axis(math.sin(roll) * 0.001* dt*dt, vec3.unit_y)
-    rot = rot * pRot * rRot
+    self.yaw = self.yaw + self.turnspeed
 
 
-    rot = rot:normalize()
     angle = self:angle()
 
     velocityVector = cpml.vec2.new(self.velocity * math.cos(angle), self.velocity * math.sin(angle))*dt
