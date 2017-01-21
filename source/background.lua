5d04d970ad6d10f7b607aa9d2e96aa8fe58c0a9c
Jaakko Hannikainen <jgke@jgke.fi>
2017-01-21 18:31:38 +0200

    Do rain with lots of raindrops

--- source/background.lua
+++ source/background.lua
@@ -4,6 +4,9 @@ background.canvas = nil
 background.rain_x_offset_per_y = 0.3
 background.rain_minlen = 10
 background.rain_maxlen = 100
+background.raindrops = {}
+background.dropcount = 3000
+background.initted = false
 
 function background:update(drawWidth, drawHeight)
     if not self.canvas or self.canvas:getWidth() ~= drawWidth or self.canvas:getHeight() ~= drawHeight then
@@ -12,15 +15,33 @@ function background:update(drawWidth, drawHeight)
     end
     love.graphics.setCanvas(self.canvas)
     love.graphics.clear(0, 0, 60)
-    for i=1,1000 do
-        local x1, y1 = math.random(-self.rain_maxlen/2,drawWidth), math.random(-self.rain_maxlen/2,drawHeight)
+    while #self.raindrops < background.dropcount do
+        local x1 = math.random(-self.rain_maxlen/2,2*drawWidth)
+        local y1 = -50
+        if not initted then
+            y1 = math.random(0,2*drawHeight)
+        end
+        local dx, dy = math.random(1, 3), math.random(30, 50)
         local len = math.random(self.rain_minlen,self.rain_maxlen)
+        table.insert(background.raindrops, {x1, y1, dx, dy, len})
+    end
+    initted = true
+    for i=#self.raindrops,1,-1 do
+        drop = self.raindrops[i]
+        drop[1] = drop[1] + drop[3]
+        drop[2] = drop[2] + drop[4]
+        x1 = drop[1]
+        y1 = drop[2]
+        len = drop[5]
         love.graphics.setColor(len*2,len*2,len*2.55)
         x2 = x1 + len*self.rain_x_offset_per_y
         y2 = y1 + len
         love.graphics.setLineStyle("rough")
         love.graphics.setLineWidth(4)
         love.graphics.line(x1, y1, x2, y2)
+        if drop[2] > 2*drawHeight  then
+            table.remove(self.raindrops, i)
+        end
     end
     love.graphics.setCanvas()
 end
