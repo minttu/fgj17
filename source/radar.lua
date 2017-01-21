7b96d3f4558e0630d570c1a3ba21eb8f5b996fa1
Jaakko Hannikainen <jgke@jgke.fi>
2017-01-21 16:04:21 +0200

    Rotate dots in radar

--- source/radar.lua
+++ source/radar.lua
@@ -65,15 +65,23 @@ function Radar.update(self, dt, ship)
     self.angle = (self.angle + self.speed*dt) % (2*math.pi)
 
     for i = 1, (#self.objects)/2 do
-        x = self.objects[2*i-1]
-        y = self.objects[2*i]
+        xx = self.objects[2*i-1]
+        yy = self.objects[2*i]
+        sa = -ship:angle() - math.pi/2
+        sin = math.sin(sa)
+        cos = math.cos(sa)
+        x = (xx - ship.location.x) * cos - (yy - ship.location.y) * sin + ship.location.x
+        y = (xx - ship.location.x) * sin + (yy - ship.location.y) * cos + ship.location.y
         dx = x - ship.location.x
         dy = y - ship.location.y
         angle = math.atan2(dy, dx) % (2*math.pi)
         if (self.angle > self.previousangle and angle >= self.previousangle and angle <= self.angle) or
             (self.angle < self.previousangle and (angle >= self.previousangle or angle <= self.angle)) then
-            table.insert(self.seenobjects, {dx, dy, 255})
-            Sounds.ui:play("radar")
+            len = math.sqrt(dx*dx + dy*dy)/3
+            if len < self.size/1.05 then
+                table.insert(self.seenobjects, {dx, dy, 255})
+                Sounds.ui:play("radar")
+            end
         end
     end
 end
@@ -93,17 +101,12 @@ function Radar.draw(self)
 
     for i = 1, (#self.seenobjects) do
         obj = self.seenobjects[i]
-        x = obj[1]/3
-        y = obj[2]/3
-        len = math.sqrt(x*x + y*y)
-        if len < self.size/1.05 then
-            x = x + self.x
-            y = y + self.y
-            a = obj[3]
-            love.graphics.setColor(255, 255, 255, a)
-            love.graphics.circle("fill", x, y, 10)
-            -- love.graphics.line(self.x, self.y, x, y)
-        end
+        x = obj[1]/3 + self.x
+        y = obj[2]/3 + self.y
+        a = obj[3]
+        love.graphics.setColor(255, 255, 255, a)
+        love.graphics.circle("fill", x, y, 10)
+        -- love.graphics.line(self.x, self.y, x, y)
     end
 
     for i = #self.seenobjects,1,-1 do
