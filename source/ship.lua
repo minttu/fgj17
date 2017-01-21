e3402880d627d3decbf7800df94702ae7e1482f3
Tuomas Kinnunen <tuomas.kinnunen@aalto.fi>
2017-01-21 22:36:24 +0200

    WAVES ADDED

--- source/ship.lua
+++ source/ship.lua
@@ -2,9 +2,11 @@ require "util"
 cpml = require "cpml"
 quat = cpml.quat
 vec3 = cpml.vec3
+mat4 = cpml.mat4
 vector = require "hump.vector"
 Sounds = require "sounds"
 Class = require "hump.class"
+
 
 Ship = Class
     { location = cpml.vec2.new(100, 100)
@@ -15,6 +17,11 @@ Ship = Class
     , turnspeed = 0
     , fuel = 1
     , fuelConsumptionMultiplier = 0.000001
+    --, seaWaveDirection = vector(1,0).rotated(-math.pi/2)
+    , waveAmplitude = 15
+    , waveSpeed = 4
+    , t = 0
+    , waveFrequency = 0.1
     }
 function Ship:init(x, y)
     self.location = cpml.vec2.new(x, y)
@@ -44,8 +51,13 @@ function Ship:getPitch()
     return self.orientation.y
 end
 
-function Ship:updateLocation(dt)
+function Ship:getWaveHeight(loc, dt)
+    --return mat4.from_angle_axis(math.cos(x*0.01)*math.pi*1.2/2+math.pi/2, vec3(1,0,0))
+    return math.cos((loc.x+dt*self.waveSpeed)*self.waveFrequency)*self.waveAmplitude
+end
 
+function Ship:updateLocation(dt)
+    self.t = self.t + dt
 
     if love.keyboard.isDown( "left" ) then
         self.turnspeed = self.turnspeed - self.turnrate*dt
@@ -75,7 +87,26 @@ function Ship:updateLocation(dt)
     velocityVector = cpml.vec2.new(self.velocity * math.cos(angle), self.velocity * math.sin(angle))*dt
     self.location = self.location + velocityVector
 
-    self.orientation = rot
+    local l = self.location
+    l = vec3(l.x, l.y, self:getWaveHeight(l,dt))
+    local xDir = vec3(self.velocity,0,0):rotate(angle, vec3(0,0,1))
+    local forwR = xDir + l
+    local forwardPoint = vec3(forwR.x,forwR.y,self:getWaveHeight(forwR,dt))
+    local forw = forwardPoint - l
+
+    local yDir = vec3(0,self.velocity,0):rotate(angle, vec3(0,0,1))
+    local leftR = yDir + l
+    local leftPoint = vec3(leftR.x,leftR.y,self:getWaveHeight(leftR,dt))
+    local left = leftPoint - l
+
+    local pitch = math.acos(xDir:dot(forw) / (xDir:len() * forw:len()))
+    local roll =  math.acos(yDir:dot(left) / (yDir:len() * left:len()))
+
+    -- Fix sign, don't know why it's not preserved by above math
+    if l.z < forwardPoint.z then pitch = -pitch end
+    if l.z < leftPoint.z then roll = -roll end
+
+    self.orientation = vector(roll,pitch)
 
     self.fuel = self.fuel - self.velocity * self.fuelConsumptionMultiplier
 end
