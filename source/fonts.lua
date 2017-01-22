e6dbbdc49ae2caee9144fd15d95cfdf7029fd530
Juhani Imberg <jimbe@rakettitiede.com>
2017-01-22 12:19:51 +0200

    bump main menu logo font size

--- source/fonts.lua
+++ source/fonts.lua
@@ -2,7 +2,7 @@ local smallFont = love.graphics.newFont(18)
 local menuFont = love.graphics.newFont("assets/fonts/impact_label/Impact Label.ttf", 32)
 local labelFont = love.graphics.newFont("assets/fonts/impact_label/Impact Label Reversed.ttf", 24)
 local labelRFont = love.graphics.newFont("assets/fonts/impact_label/Impact Label.ttf", 24)
-local bigMenuFont = love.graphics.newFont("assets/fonts/impact_label/Impact Label Reversed.ttf", 128)
+local bigMenuFont = love.graphics.newFont("assets/fonts/impact_label/Impact Label Reversed.ttf", 256)
 
 
 return {
